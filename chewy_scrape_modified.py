196fc9d76ccec4564bc6933eaf8a4fa2e8239405
Ethan Bard <ebard@thebardfamily.com>
2022-10-30 02:20:06 -0400

    update scrapepages data types

--- chewy_scrape_modified.py
+++ chewy_scrape_modified.py
@@ -10,7 +10,6 @@ import json
 prod_links = []
 prod_dict = {}
 
-
 def GetLinks(url):
     page = get(url, timeout=5, headers={'User-Agent': 'SomeAgent 1.0'})
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -54,7 +53,7 @@ def LoadLinks(file):
 
 
 def ScrapePages(pages):
-    for link in prod_links[0:2]:
+    for link in prod_links:
         while True:
             try:
                 print(f"Trying: {link}")
@@ -63,10 +62,16 @@ def ScrapePages(pages):
                 soup = BeautifulSoup(page.content, 'html.parser')
                 prod_name = soup.find("div", {"class": "_2lMe8l0Qayns-EKWOOJfXh"}).find('h1').text
                 print(prod_name)
+                rec_rate = soup.find('div', {"data-testid": "product-recommendation-rate"}).find('div').text.replace("%","")
+                rating = soup.find('div',{"data-testid": "product-reviews-rating"}).text
+                review_count = soup.find('div',{"data-testid": "product-reviews-count"}).text.split(" ")[0]
                 specifications = soup.find('div', {"class": "_3xer4bkNDgAik73jS-7m94"}).find('table').find_all('tr')
                 specifications_dict = {}
                 specifications_dict['name'] = prod_name
                 specifications_dict['Ingredients'] = soup.find('section', id="INGREDIENTS-section").find('p').text 
+                specifications_dict['Reccomendation Rate'] = int(rec_rate)
+                specifications_dict['Rating'] = float(rating)
+                specifications_dict['Review Count'] = int(review_count)
                 for i in range(0, len(specifications)):
                     specifications_dict[specifications[i].find('th').text] = specifications[i].find('td').text
                 prod_dict[specifications_dict['Item Number']] = specifications_dict
@@ -84,7 +89,7 @@ LoadLinks("chewy\chewyLinks.txt")
 ScrapePages(prod_links)
 
 prod_json = json.dumps(prod_dict, indent = 4)
-pprint.pprint(prod_dict)
+#pprint.pprint(prod_dict)
 
 with open("chewy\sample.json", "w") as outfile:
     outfile.write(prod_json)
