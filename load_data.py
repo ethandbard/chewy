b74a5725c9725cced0583ef118e3ca3be3ec80b2
Ethan Bard <ebard@thebardfamily.com>
2022-10-30 23:32:44 -0400

    function update, json_test_dump

---
+++ load_data.py
@@ -0,0 +1,9 @@
+from chewy_functions import *
+import numpy as np
+import pandas as pd
+
+dict = LoadJson("chewy\\test_dump.json")
+
+pprint.pprint(dict)
+
+
--- scrape_pages.py
+++ scrape_pages.py
@@ -4,10 +4,10 @@ from chewy_functions import *
 LoadLinks("chewy\chewy_links_test.txt")
 
 #get data from each page
-ScrapePages(prod_links[0:100])
+ScrapePages(prod_links[0:2])
 
 #save dict to json
 prod_json = json.dumps(prod_dict, indent = 4)
 
 #write .json 
-WriteJson("chewy\\test_dump.json")
+SaveJson(prod_json, "chewy\\test_dump.json")
